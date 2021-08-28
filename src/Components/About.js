64c9f901d206c0f156293466aa3cce7192bc5dca
Prajwal Bodhale <60910198+prajwal1258@users.noreply.github.com>
2021-08-28 18:58:09 +0530

    updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,76 +1,82 @@
 import React,{useState} from 'react'
 
 export default function About() {
-    const [myStyle, setMyStyle] = useState({
-        color:`black`,
-        backgroundColor:`white`
-    })
-    const [btntext, setBtntext] = useState("Enable Dark Mode")
-    const toggleStyle = ()=>{
-        if(myStyle.color === `black`){
-            setMyStyle({
-                color:`white`,
-                backgroundColor:`black`,
-                border: `1px solid white`
-            
-            })
-            setBtntext("Enable Light Mode")
-        }else{
-            setMyStyle({
-                color:`black`,
-                backgroundColor:`white`
-    
-                })
-                setBtntext("Enable Dark Mode")
-            
-        }
+//     const [myStyle, setMyStyle] = useState({
+//         color:`black`,
+//         backgroundColor:`white`
+//     })
+    let myStyle = {
+      color:props.mode ==='dark'?'white':'#042743',
+      backgroundColor:props.mode ==='dark'?'rgb(36 74 104)':'white',
     }
+    
+    
+//     const [btntext, setBtntext] = useState("Enable Dark Mode")
+//     const toggleStyle = ()=>{
+//         if(myStyle.color === `black`){
+//             setMyStyle({
+//                 color:`white`,
+//                 backgroundColor:`black`,
+//                 border: `1px solid white`
+            
+//             })
+//             setBtntext("Enable Light Mode")
+//         }else{
+//             setMyStyle({
+//                 color:`black`,
+//                 backgroundColor:`white`
+    
+//                 })
+//                 setBtntext("Enable Dark Mode")
+            
+//         }
+//     }
 
     return (
        
-    <div className="container" style={myStyle}>
-        <h2 className="my-3">About Us</h2>
+    <div className="container">
+        <h2 className="my-3" style={{color:props.mode ==='dark'?'white':'#042743'}}>About Us</h2>
       <div className="accordion"  id="accordionExample">
-    <div className="accordion-item" style={myStyle}>
+    <div className="accordion-item">
         <h2 className="accordion-header"  id="headingOne">
             <button className="accordion-button" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
             <strong>Analyze Your text</strong>
             </button>
         </h2>
-        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+        <div id="collapseOne" className="accordion-collapse collapse show" style={myStyle} aria-labelledby="headingOne" data-bs-parent="#accordionExample">
             <div className="accordion-body">
-                Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count.
+            Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or
             </div>
         </div>
     </div>
-    <div className="accordion-item" style={myStyle}>
+    <div className="accordion-item">
         <h2 className="accordion-header" id="headingTwo">
             <button className="accordion-button collapsed"style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
             <strong>Free to use</strong>
         </button>
         </h2>
-        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+        <div id="collapseTwo" className="accordion-collapse collapse" style={myStyle} aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
         <div className="accordion-body">
             TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
         </div>
     </div>
   </div>
-  <div className="accordion-item" style={myStyle}>
+  <div className="accordion-item">
     <h2 className="accordion-header" id="headingThree">
       <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        <strong>Browser Compatible</strong> 
+      <strong>Browser Compatible</strong> 
       </button>
     </h2>
-    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+    <div id="collapseThree" className="accordion-collapse collapse" style={myStyle} aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div className="accordion-body">
         This word counter software works in any web browsers such as Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to count characters in facebook, blog, books, excel document, pdf document, essays, etc.
       </div>
     </div>
   </div>
   </div>
-  <div className="container my-3">
+  {/* <div className="container my-3">
    <button onClick={toggleStyle}type="button" className="btn btn-outline-danger">{btntext}</button>    
-  </div>
+  </div> */}
    
 </div>
      
